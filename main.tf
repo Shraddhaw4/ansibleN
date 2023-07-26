c2abc74f899af32654a2fc84c3b4596c47971c07
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-07-26 13:52:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,17 @@ resource "null_resource" "key-ownership" {
     command = "sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/Jenkins-Server.pem"
   }
 }
+resource "null_resource" "Transfer_ssh1" {
+  depends_on = [null_resource.key]
+  provisioner "local-exec" {
+    on_failure = fail
+    command = "sudo echo 'Host *\n\tStrictHostKeyChecking no\n\tUser ubuntu\n\tIdentityFile /home/ubuntu/.ssh/Jenkins-Server.pem' > config"
+  }
+}
+resource "null_resource" "Transfer_ssh2" {
+  depends_on = [null_resource.Transfer_ssh1]
+  provisioner "local-exec" {
+    on_failure = fail
+    command = "sudo cp config /home/ubuntu/.ssh/config"
+  }
+}

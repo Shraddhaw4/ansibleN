33c7fe868a9a2d0cba8263a3ec4a1b225531cf94
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-07-26 17:02:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,48 @@ resource "null_resource" "Transfer_ssh2" {
     command = "sudo cp config /home/ubuntu/.ssh/config"
   }
 }
+#---------------Creating Inventory-------------------------------
+resource "null_resource" "inventory" {
+  provisioner "local-exec" {
+    on_failure  = fail
+    command = "echo '[servers]' > inventory"
+  }
+}
+#-----------------Fetching Ubuntu AMI------------------------------
+data "aws_ami" "ubuntu-ami" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+#-----------------Ansibe Host---------------------------------------
+resource "aws_instance" "ansible-hosts" {
+  ami = data.aws_ami.ubuntu-ami.id
+  instance_type = "t2.micro"
+  key_name = "Jenkins-Server"
+
+  tags = {
+    Name = "terr-ansible-host"
+  }
+}
+#----------------Inventory File--------------------------------------
+resource "null_resource" "inventory-file" {
+  depends_on = [aws_instance.ansible-hosts,null_resource.inventory]
+  provisioner "local-exec" {
+    on_failure = fail
+    command = "echo ${aws_instance.ansible-hosts.tags["Name"]} ansible_host=${aws_instance.ansible-hosts.public_ip} ansible_connection=ssh ansible_user=ubuntu >> inventory"
+  }
+}
+resource "null_resource" "ping" {
+  depends_on = [null_resource.inventory-file, aws_instance.ansible-hosts]
+  provisioner "local-exec" {
+    on_failure = fail
+    command = "ansible servers -m ping -i inventory"
+  }
+}

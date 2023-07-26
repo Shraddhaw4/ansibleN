09fc29aec538cfa6ff5ceb869134e1f81109703c
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-07-26 13:06:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,17 @@
 provider "aws" {
   region                   = "ap-south-1"
 }
-#-------------------Test----------------------------------------------
-resource "null_resource" "hell0" {
+#-------------------PasswordlessSSH----------------------------------------------
+resource "null_resource" "key" {
   provisioner "local-exec" {
-    on_failure = fail
-    command = "echo Hello World"
+    on_failure  = fail
+    command = "sudo cp /var/tmp/Jenkins-Server.pem /home/ubuntu/.ssh/Jenkins-Server.pem"
+  }
+}
+resource "null_resource" "key-ownership" {
+  depends_on = [null_resource.key]
+  provisioner "local-exec" {
+    on_failure  = fail
+    command = "sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/Jenkins-Server.pem"
   }
 }

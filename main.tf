4f0e6977cedaa2b4bd30b85b7632b9fb8af2af3d
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-07-31 18:44:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@
 provider "aws" {
   region                   = "ap-south-1"
 }
+resource "aws_instance" "ansible-test" {
   ami           = "ami-08e5424edfe926b43"
   key_name      = "newkey"
   instance_type = "t2.micro"
   tags = {
-    Name = "Ansible-master"
+    Name = "Ansible-master-1"
   }
 
  

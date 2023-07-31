b181607f20d14eb57637880f1b3b25f0ead06347
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-07-31 18:35:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,69 +2,31 @@
 provider "aws" {
   region                   = "ap-south-1"
 }
-#-------------------PasswordlessSSH----------------------------------------------
-resource "null_resource" "key" {
-  provisioner "local-exec" {
-    on_failure  = fail
-    command = "sudo cp /var/tmp/Demo_ans_key.pem /home/ubuntu/.ssh/Demo_ans_key.pem"
-  }
-}
-resource "null_resource" "Transfer_ssh1" {
-  depends_on = [null_resource.key]
-  provisioner "local-exec" {
-    on_failure = fail
-    command = "sudo echo 'Host *\n\tStrictHostKeyChecking no\n\tUser ubuntu\n\tIdentityFile /home/ubuntu/.ssh/Demo_ans_key.pem' > config"
-  }
-}
-resource "null_resource" "Transfer_ssh2" {
-  depends_on = [null_resource.Transfer_ssh1]
-  provisioner "local-exec" {
-    on_failure = fail
-    command = "sudo cp config /home/ubuntu/.ssh/config"
-  }
-}
-#---------------Creating Inventory-------------------------------
-resource "null_resource" "inventory" {
-  provisioner "local-exec" {
-    on_failure  = fail
-    command = "echo '[servers]' > inventory"
-  }
-}
-#-----------------Fetching Ubuntu AMI------------------------------
-data "aws_ami" "ubuntu-ami" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-#-----------------Ansibe Host---------------------------------------
-resource "aws_instance" "ansible-hosts" {
-  ami = data.aws_ami.ubuntu-ami.id
+  ami           = "ami-08e5424edfe926b43"
+  key_name      = "newkey"
   instance_type = "t2.micro"
-  key_name = "Demo_ans_key"
-
   tags = {
-    Name = "terr-ansible-host"
+    Name = "Ansible-master"
   }
-}
-#----------------Inventory File--------------------------------------
-resource "null_resource" "inventory-file" {
-  depends_on = [aws_instance.ansible-hosts,null_resource.inventory]
-  provisioner "local-exec" {
+
+ 
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'hello' > test.txt",
+    ]
     on_failure = fail
-    command = "echo ${aws_instance.ansible-hosts.tags["Name"]} ansible_host=${aws_instance.ansible-hosts.public_ip} ansible_connection=ssh ansible_user=ubuntu >> inventory"
-  }
-}
-resource "null_resource" "ping" {
-  depends_on = [null_resource.inventory-file, aws_instance.ansible-hosts]
-  provisioner "local-exec" {
-    on_failure = fail
-    command = "ansible servers -m ping -i inventory"
+
+ 
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/var/tmp/newkey.pem")
+      host        = self.public_ip
+
+ 
+
+    }
   }
 }
